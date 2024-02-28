ea61300db12bff50aa4b18fcc094ae446a40ba64
Aamir Sohail KmAs <AamirSohailKmAs@gmail.com>
2024-02-28 10:43:13 +0500

    update defaults

--- server/src/support/defaults.ts
+++ server/src/support/defaults.ts
@@ -3,7 +3,8 @@ import { CompletionList } from 'vscode-languageserver';
 export const EMPTY_COMPLETION_LIST: CompletionList = CompletionList.create();
 export const QUEUE_CHUNK_SIZE: number = 10000;
 export const INDEX_DELAY = 500;
-export const DEFAULT_MAX_FILE_SIZE = 1000000;
+export const DEFAULT_PHP_VERSION = 803;
+export const DEFAULT_MAX_FILE_SIZE = 1024 * 1024;
 export const DEFAULT_EXCLUDE = [
     '**/.git/**',
     '**/.svn/**',
@@ -16,3 +17,4 @@ export const DEFAULT_EXCLUDE = [
     '**/.history/**',
     '**/vendor/**/vendor/**',
 ];
+

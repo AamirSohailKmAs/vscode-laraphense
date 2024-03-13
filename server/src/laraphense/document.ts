9145622f85352109117e568807c53e572ba2f2aa
Aamir Sohail KmAs <AamirSohailKmAs@gmail.com>
2024-03-13 19:22:17 +0500

    update: flat document to have TextDocument

--- server/src/laraphense/document.ts
+++ server/src/laraphense/document.ts
@@ -2,6 +2,8 @@
 
 import { DocumentUri } from 'vscode-languageserver';
 import { Debounce } from '../support/debounce';
+import { TextDocument } from 'vscode-languageserver-textdocument';
+import { toDocLang } from '../helpers/uri';
 
 export enum DocLang {
     html = 'html',
@@ -15,18 +17,24 @@ export enum DocLang {
 }
 
 export class FlatDocument {
+    static fromTextDocument(doc: TextDocument): FlatDocument {
+        return new FlatDocument(doc.uri, toDocLang(doc.languageId), doc.version, doc.getText());
+    }
+
+    public doc: TextDocument;
     public createdAt: number;
     public lastCompile: [number, number] = [0, 0];
     compileDebounce?: Debounce<unknown, unknown>;
     diagnoseDebounce?: Debounce<unknown, unknown>;
     constructor(
-        public uri: DocumentUri,
+        uri: DocumentUri,
         public languageId: DocLang,
-        public version: number,
-        public content: string,
+        version: number,
+        content: string,
         public isOpened: boolean = false,
         createdAt?: number
     ) {
+        this.doc = TextDocument.create(uri, languageId, version, content);
         this.createdAt = createdAt ?? Date.now();
         this.lastCompile = process.hrtime();
     }

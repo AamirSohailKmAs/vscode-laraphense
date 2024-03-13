6e0e6576ffa11a4b2ea7d86ceec72cd4d707383f
Aamir Sohail KmAs <AamirSohailKmAs@gmail.com>
2024-03-13 19:26:03 +0500

    update blade Ast types structure

--- server/src/types/bladeAst.ts
+++ server/src/types/bladeAst.ts
@@ -2,27 +2,82 @@
 
 import { Program } from 'php-parser';
 
-export type Comment = {
-    value: string;
-};
+export type Position = { line: number; character: number; offset: number };
 
-export type ErrorMessage = {
-    value: string;
-};
+export enum TokenKind {
+    EOF = 'end of file', // end of file
 
-export type HtmlAttribute = {
-    name: string;
-    value: string;
-};
+    JS = 'js',
+    PHP = 'php',
+    CSS = 'css',
+    HTML = 'html',
+    TEXT = 'TEXT',
 
-export type Element = {
-    tag: string;
-    attributes: HtmlAttribute[];
-    children: Element[];
-};
+    DIRECTIVE = 'directive',
 
-export class Tree {
-    kind = 'tree';
-    constructor(public children: Array<Element | Program>, public errors: ErrorMessage[] = []) {}
+    TAG_NAME = 'tag name',
+    ATTRIBUTE = 'attribute',
+    ATTRIBUTE_VALUE = 'attribute Value',
+    HTML_COMMENT = 'html comment',
+    // HTML_COMMENT_START = 'HTML_COMMENT_START', // <!--
+    // HTML_COMMENT_END = 'HTML_COMMENT_END', // -->
+
+    // PHP_TAG_START = '<?php', // auto complete these
+    // PHP_TAG_END = '?>', // auto complete these
+    // PHP_ECHO_START = '<?=', // auto complete these
+    BLADE_COMMENT = 'blade comment',
+    BLADE_COMMENT_START = '{{--',
+    BLADE_COMMENT_END = '--}}',
+    BLADE_RAW_END = '!!}',
+    BLADE_RAW_START = '{!!',
+    BLADE_ECHO_START = '{{',
+    BLADE_ECHO_END = '}}',
+
+    EQUAL = '=', // =
+    ANGLE_OPEN = '<',
+    ANGLE_CLOSE = '>',
+    SLASH = '/',
+    QUOTE = `' or "`,
 }
+
+export type Token = {
+    kind: TokenKind;
+    value: string;
+    afterWhitespace: boolean;
+    pos: Position;
+    end?: Position;
+};
+
+export type Location = {
+    start: Position;
+    end: Position;
+};
+
+export interface Attribute extends AstNode {
+    kind: 'htmlAttribute';
+    name: string;
+    value?: string;
+}
+export interface HtmlElement extends AstNode {
+    attributes?: Attribute[];
+    content?: string;
+    selfClosing: boolean;
+}
+export interface AstNode {
+    name: string;
+    kind: 'tree' | 'element' | 'language' | 'openTag' | 'closeTag' | 'errorNode' | 'htmlAttribute';
+    children?: AstNode[];
+    loc: Location;
+}
+
+export interface ErrorNode extends AstNode {
+    kind: 'errorNode';
+    message: string;
+}
+
+export type Tree = {
+    kind: 'tree';
+    children: Array<AstNode | Program>;
+    errors: Array<ErrorNode>;
+};
 

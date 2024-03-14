393b3aea8a948163fb913a82c23a4cc6676d486a
Aamir Sohail KmAs <AamirSohailKmAs@gmail.com>
2024-03-14 13:27:02 +0500

    add: location, position, range converting functions

--- server/src/helpers/symbol.ts
+++ server/src/helpers/symbol.ts
@@ -1,13 +1,21 @@
 'use strict';
 
-import { Location, Position as ParserPosition } from 'php-parser';
-import { Position, Range } from 'vscode-languageserver';
+import { Location as ParserLocation, Position as ParserPosition } from 'php-parser';
+import { Position as LSPPosition, Range } from 'vscode-languageserver';
+import { Location, Position } from '../types/bladeAst';
 
-export function toLSPRange(location: Location): Range {
+export function toPosition(pos: ParserPosition): Position {
+    return { offset: pos.offset, line: pos.line, character: pos.column + 1 };
+}
+
+export function toLocation(loc: ParserLocation): Location {
+    return { start: toPosition(loc.start), end: toPosition(loc.end) };
+}
+
+export function toLSPRange(location: ParserLocation): Range {
     return Range.create(toLSPPosition(location.start), toLSPPosition(location.end));
 }
 
 export function toLSPPosition(position: ParserPosition) {
-    return Position.create(position.line, position.column);
+    return LSPPosition.create(position.line, position.column);
 }
-

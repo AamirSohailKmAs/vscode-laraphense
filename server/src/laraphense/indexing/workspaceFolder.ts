1a1856777afc1949c66517392d83eac38de1eebb
Aamir Sohail KmAs <AamirSohailKmAs@gmail.com>
2024-03-14 13:41:34 +0500

    update: workspace folder

--- server/src/laraphense/indexing/workspaceFolder.ts
+++ server/src/laraphense/indexing/workspaceFolder.ts
@@ -1,6 +1,14 @@
 'use strict';
 import { glob } from 'fast-glob';
-import { pathToUri, uriToPath } from '../../helpers/uri';
+import { folderContainsUri, pathToUri, uriToPath } from '../../helpers/uri';
+import { FolderUri } from '../../types/general';
+import { SymbolTable } from './tables/symbolTable';
+import { DEFAULT_INCLUDE, DEFAULT_EXCLUDE } from '../../support/defaults';
+
+export const enum FolderKind {
+    User = 0,
+    Stub = 1,
+}
 
 export type FileEntry = {
     uri: string;
@@ -9,34 +17,55 @@ export type FileEntry = {
 };
 
 export class WorkspaceFolder {
-    private _uri: string;
-    private _includeGlobs: string[];
-    private _excludeGlobs: string[];
+    public symbolTable: SymbolTable;
+    constructor(
+        private _uri: string,
+        private _kind: FolderKind = FolderKind.User,
+        private _includeGlobs: string[] = DEFAULT_INCLUDE,
+        private _excludeGlobs: string[] = DEFAULT_EXCLUDE
+    ) {
+        this._includeGlobs = _includeGlobs.map(this.uriToGlobPattern);
+        this._excludeGlobs = this._excludeGlobs.map(this.uriToGlobPattern);
 
-    constructor(uri: string, includeGlobs: string[], excludeGlobs: string[]) {
-        this._uri = uri;
-        this._includeGlobs = includeGlobs.map(this.uriToGlobPattern);
-        this._excludeGlobs = excludeGlobs.map(this.uriToGlobPattern);
         if (this._uri.slice(-1) === '/') {
             this._uri = this._uri.slice(0, -1);
         }
+        this.symbolTable = new SymbolTable();
     }
 
-    public get uri(): string {
+    public get uri(): FolderUri {
         return this._uri;
     }
+
+    public get kind(): FolderKind {
+        return this._kind;
+    }
+
     public get includeGlobs(): string[] {
         return this._includeGlobs;
     }
+
     public get excludeGlobs(): string[] {
         return this._excludeGlobs;
     }
 
+    public get isStubs() {
+        return this._kind === FolderKind.Stub;
+    }
+
+    public get vendorUri() {
+        return this._uri + '/vendor';
+    }
+
+    public isVendorFile(uri: string) {
+        return uri.indexOf('/vendor/') !== -1;
+    }
+
     public async findFiles() {
-        const entries = await glob(uriToPath(this._uri) + '/**', {
+        const entries = await glob(this._includeGlobs, {
             stats: true,
-            cwd: uriToPath(this._uri),
-            ignore: this._excludeGlobs.map(this.uriToGlobPattern),
+            cwd: uriToPath(this._uri) + '/',
+            ignore: this._excludeGlobs,
             absolute: true,
             dot: true,
             suppressErrors: true,
@@ -49,12 +78,16 @@ export class WorkspaceFolder {
         }));
     }
 
-    public uriToGlobPattern(uri: string) {
-        if (uri.indexOf('/') === -1) {
-            return '**/' + uri;
+    public uriToGlobPattern(name: string) {
+        if (name.indexOf('/') === -1) {
+            return '**/' + name + '/*.{php,blade.php}';
         } else {
-            return uri;
+            return name;
         }
+    }
+
+    contains(folder: string) {
+        return folderContainsUri(folder, this._uri);
     }
 }
 

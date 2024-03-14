82fe6fb0d54db5a034e43f34f2b2010e3eedde92
Aamir Sohail KmAs <AamirSohailKmAs@gmail.com>
2024-03-14 13:44:16 +0500

    update: indexer uses compiler

--- server/src/laraphense/indexing/indexer.ts
+++ server/src/laraphense/indexing/indexer.ts
@@ -1,33 +1,37 @@
 'use strict';
 
-import { Queue } from '../../support/queue';
-import { Workspace } from './workspace';
-
-enum QueueAction {
-    Index,
-    Forget,
-}
-
+import { laraphenseRc } from '../../languages/baseLang';
+import { Compiler } from '../compiler';
+import { WorkspaceFolder } from './workspaceFolder';
 export class Indexer {
-    private queue: Queue<{ uri: string; action: QueueAction }>;
-    private _isIndexing: boolean = false;
-    constructor(private _workspace: Workspace, private _maxFileSize: number) {
-        this.queue = new Queue();
-    }
+    private _indexCount: number = 0;
 
-    get maxFileSize() {
-        return this._maxFileSize;
-    }
+    constructor(private _compiler: Compiler, private config: laraphenseRc) {}
 
-    set maxFileSize(size: number) {
-        this._maxFileSize = size;
-    }
+    public async indexFolder(folder: WorkspaceFolder) {
+        const files = await folder.findFiles();
 
-    public indexWorkspace(openUris: string[]) {
-        console.log('openUris', openUris);
+        console.log(`indexFolder [${folder.uri}] having files [${files.length}]`);
 
-        // this._workspace.folders.forEach(this.indexFolder.bind(this));
-    }
+        if (files.length < 1) {
+            return;
+        }
 
+        for (let i = 0; i < files.length; ++i) {
+            const entry = files[i];
+            if (entry.size > this.config.maxFileSize) {
+                console.warn(
+                    `${entry.uri} has ${entry.size} bytes which is over the maximum file size of ${this.config.maxFileSize} bytes.`
+                );
+                continue;
+            }
+
+            const compiled = this._compiler.compileUri(entry.uri, folder);
+            if (!compiled) {
+                continue;
+            }
+            this._indexCount++;
+        }
     }
+}
 

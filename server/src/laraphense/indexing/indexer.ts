b71e1041d7533af87356c6b50f81c322a4e5d750
Aamir Sohail KmAs <AamirSohailKmAs@gmail.com>
2024-03-21 13:35:52 +0500

    track missing files in indexer

--- server/src/laraphense/indexing/indexer.ts
+++ server/src/laraphense/indexing/indexer.ts
@@ -17,20 +17,32 @@ export class Indexer {
             return;
         }
 
+        const missingFiles: Array<{ uri: string; reason: string }> = [];
+
+        let count = 0;
+
         for (let i = 0; i < files.length; ++i) {
             const entry = files[i];
             if (entry.size > this.config.maxFileSize) {
                 console.warn(
                     `${entry.uri} has ${entry.size} bytes which is over the maximum file size of ${this.config.maxFileSize} bytes.`
                 );
+                count++;
                 continue;
             }
 
             const compiled = this._compiler.compileUri(entry.uri, folder);
             if (!compiled) {
+                missingFiles.push({ uri: entry.uri, reason: 'can not compile' });
                 continue;
             }
+            count++;
             this._indexCount++;
+        }
+
+        console.log(`folder [${folder.uri}] indexing completed with files [${count}]`);
+        if (missingFiles.length > 0) {
+            console.log('missingFiles', missingFiles);
         }
     }
 }
